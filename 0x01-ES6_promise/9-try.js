0e39d5ef1a813430e8c4ac0d196a7a1c00c9de13
geno <yvesgeno@outlook.com>
2024-01-04 23:54:56 +0200

    task 9 correction

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,11 +2,11 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    queue.concat(mathFunction());
+    queue.push(mathFunction());
   } catch (err) {
-    queue.concat(String(err));
+    queue.push(String(err));
   } finally {
-    queue.concat('Guardrail was processed');
+    queue.push('Guardrail was processed');
   }
 
   return queue;
