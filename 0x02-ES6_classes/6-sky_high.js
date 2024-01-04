f2b92c77fd6125157133fd1180873a0d9bf0ad88
geno <yvesgeno@outlook.com>
2024-01-05 00:25:27 +0200

    task 9

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
@@ -19,5 +19,3 @@ class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this._floors} floors.`;
   }
 }
-
-export default SkyHighBuilding;
