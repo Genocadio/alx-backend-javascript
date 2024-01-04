3fbd5683b022cf9cfe009fdd95fe9ca9ee33bd4b
geno <yvesgeno@outlook.com>
2024-01-04 22:21:49 +0200

    task 0 correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,13 @@
 export default function taskBlock(trueOrFalse) {
  const task = false;
  const task2 = true;
+
  if (trueOrFalse) {
-  const taskInner = true;
-  const task2Inner = false;
+   // eslint-disable-next-line no-unused-vars, no-shadow
+   const task = true;
+   // eslint-disable-next-line no-unused-vars, no-shadow
+   const task2 = false;
  }
+
  return [task, task2];
 }
--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
-export default class NeighborhoodsList {
-    constructor() {
-        this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    const self = this;
+    this.addNeighborhood = (newNeighborhood) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
     }
-
-    addNeighborhood = (newNeighborhood) => {
-        this.sanFranciscoNeighborhoods.push(newNeighborhood);
-        return this.sanFranciscoNeighborhoods;
-    };
-}
+  }
