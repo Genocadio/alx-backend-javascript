ba574b73789b8b0c339f249b29865ebc8ff653cf
geno <yvesgeno@outlook.com>
2024-01-09 22:47:05 +0200

    task 1 correction

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import getListStudents from './0-get_list_students';
-
-export default function getListStudentIds(getListStudents) {
-  const listStudents = getListStudents();
+export default function getListStudentIds(listofStudents) {
   if (!Array.isArray(listStudents)) return [];
   return listStudents.map((student) => student.id);
 }
