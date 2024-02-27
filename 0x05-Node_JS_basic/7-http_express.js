975611c161cdfab31c4c8978617f09b51f1b3494
geno <yvesgeno@outlook.com>
2024-02-28 00:26:31 +0200

    Refactor code to improve performance and readability

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { countStudents } = require('./3-read_file_async');
+
+const PORT = 1245;
+const DATABASE_PATH = 'database.csv';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.type('text').send('Hello Holberton School!\n');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(DATABASE_PATH)
+    .then(() => {
+      res.type('text').send('This is the list of our students\n');
+    })
+    .catch((error) => {
+      res.status(500).send(`${error.message}\n`);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
+});
+
+module.exports = app;
