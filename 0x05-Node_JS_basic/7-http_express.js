7e866796a7bfa171eeb655c0509b1ec1c7fbc908
geno <yvesgeno@outlook.com>
2024-02-28 00:32:07 +0200

    Update response messages in Express routes

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -7,13 +7,13 @@ const DATABASE_PATH = 'database.csv';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.type('text').send('Hello Holberton School!\n');
+  res.type('text').send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   countStudents(DATABASE_PATH)
     .then(() => {
-      res.type('text').send('This is the list of our students\n');
+      res.type('text').send('This is the list of our students');
     })
     .catch((error) => {
       res.status(500).send(`${error.message}\n`);
