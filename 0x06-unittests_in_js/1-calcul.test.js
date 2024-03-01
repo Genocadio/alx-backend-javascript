3e1a29536e75e4bc7c28ad77e0955ed1cb507898
geno <yvesgeno@outlook.com>
2024-03-01 23:23:00 +0200

    task 1

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,59 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+    it('should return the sum of two numbers', function() {
+            assert.equal(calculateNumber('SUM', 1, 2), 3);
+        });
+    it('should handle negative numbers', function() {
+            assert.equal(calculateNumber('SUM', -1, -2), -3);
+        }
+    );
+    it('should round two floats', function() {
+            assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+        }
+    );
+    it('should handle one first float', function() {
+            assert.equal(calculateNumber('SUM', 1.5, 3), 5);
+        }
+    );
+    it('should handle last floats', function() {
+            assert.equal(calculateNumber('SUM', 1, 3.5), 5);
+        }
+    );
+    it('should handle zero', function() {
+            assert.equal(calculateNumber('SUM', 0, 0), 0);
+        }
+    );
+    it('should return the difference of two numbers', function() {
+            assert.equal(calculateNumber('SUBTRACT', 1, 2), -1);
+        });
+    it('should handle negative numbers', function() {
+            assert.equal(calculateNumber('SUBTRACT', -1, -2), 1);
+        }
+    );
+    it('should round two floats', function() {
+            assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+        }
+    );
+    it('should handle one first float', function() {
+            assert.equal(calculateNumber('SUBTRACT', 1.5, 3), -1);
+        }
+    );
+    it('should handle last floats', function() {
+            assert.equal(calculateNumber('SUBTRACT', 1, 3.5), -3);
+        }
+    );
+    it('should return the division of two numbers', function() {
+            assert.equal(calculateNumber('DIVIDE', 1, 2), 0.5);
+        });
+    it('should handle negative numbers', function() {
+            assert.equal(calculateNumber('DIVIDE', -1, -2), 0.5);
+        }
+    );
+    it('should round two floats', function() {
+            assert.equal(calculateNumber('DIVIDE', 1.2, 3.7), 0.5);
+        }
+    );
+}
+);
